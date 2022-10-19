3a29ebced720dd020a02a8d4eb27a2a372e655db
kachamkin <kachamkin@gmail.com>
2022-10-19 18:41:38 +0300

    Filter

--- WebSignTool/Filter.cs
+++ WebSignTool/Filter.cs
@@ -13,27 +13,25 @@ namespace WebSignTool
 
                 string certDir = Global.GetCertDir();
 
-                using (FileStream fs = new(certDir + "\\" + "log.txt", FileMode.OpenOrCreate))
+                using FileStream fs = new(certDir + "\\" + "log.txt", FileMode.OpenOrCreate);
+                fs.Seek(0, SeekOrigin.End);
+
+                using StreamWriter bw = new(fs);
+                bw.Write(DateTime.Now + " ");
+                string HostName = "";
+                try
                 {
-                    fs.Seek(0, SeekOrigin.End);
-                    using (StreamWriter bw = new(fs))
-                    {
-                        bw.Write(DateTime.Now + " ");
-                        string HostName = "";
-                        try
-                        {
-                            HostName = context.HttpContext.Connection.RemoteIpAddress == null ? "" : " (" + Dns.GetHostEntry(context.HttpContext.Connection.RemoteIpAddress).HostName + ")";
-                        }
-                        catch (Exception ex)
-                        {
-                            HostName = " (" + ex.Message + ")";
-                        }
-                        bw.Write(context.HttpContext.Connection.RemoteIpAddress + HostName + " ");
-                        bw.Write(context.HttpContext.Request.Path + "\r\n");
-                        bw.Close();
-                    }
-                    fs.Close();
+                    HostName = context.HttpContext.Connection.RemoteIpAddress == null ? "" : " (" + Dns.GetHostEntry(context.HttpContext.Connection.RemoteIpAddress).HostName + ")";
                 }
+                catch (Exception ex)
+                {
+                    HostName = " (" + ex.Message + ")";
+                }
+                bw.Write(context.HttpContext.Connection.RemoteIpAddress + HostName + " ");
+                bw.Write(context.HttpContext.Request.Path + "\r\n");
+
+                bw.Close();
+                fs.Close();
             }
             catch
             {
